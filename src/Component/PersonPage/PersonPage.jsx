93e5f50b207c85fef4c0e3a6c04617907482c899
lexent3010 <lexent.lex@gmail.com>
2020-03-27 01:03:38 +0300

    Полный финиш! (withoutURL)

--- src/Component/PersonPage/PersonPage.jsx
+++ src/Component/PersonPage/PersonPage.jsx
@@ -10,36 +10,36 @@ const PersonPage = (props) => {
                     <div className={style.backBlock} onClick={props.back}>back</div>
                     <div className={style.personBlock}>
                         <div className={style.personAvatar_selectButton}>
-                            <div className={style.previousButton}>{(() => {
-                                return props.checkBox === 'ok' || props.checkBox === 'notNext' ?
+                            <div className={style.previousButton}>{
+                                props.checkBox === 'ok' || props.checkBox === 'notNext' ?
                                     <div className={style.button} onClick={() => {
                                         props.changePerson('PREVIOUS')
                                     }}>
                                         <img src={require('../../images/site/1.svg')} alt="&lt;"/>
                                     </div> : null
-                            })()}</div>
+                            }</div>
                             {props.currentPerson.map(person =>
                                 <div className={style.personAvatar_subjectsCount} key={person.id}>
                                     <img className={style.personAvatar} src={require('../../images/' + person.image)}
                                          alt={person.name}/>
-                                    {(() => {
-                                        return person.subjectsCount !== 0 && props.person[0].parent !== undefined ?
+                                    {
+                                        person.subjectsCount !== 0 && props.person[0].parent !== undefined ?
                                             <div className={style.personSubjectsCount}>
                                                 <img className={style.subjectBorder}
                                                      src={require('../../images/site/subjectBorder.png')} alt=''/>
                                                 <div className={style.subjectsCount}>{person.subjectsCount}</div>
                                             </div>
                                             : null
-                                    })()}
+                                    }
                                 </div>)}
-                            <div className={style.nextButton}>{(() => {
-                                return props.checkBox === 'ok' || props.checkBox === 'notPrevious' ?
+                            <div className={style.nextButton}>{
+                                props.checkBox === 'ok' || props.checkBox === 'notPrevious' ?
                                     <div className={style.button} onClick={() => {
                                         props.changePerson('NEXT')
                                     }}>
                                         <img src={require('../../images/site/2.svg')} alt="&gt;"/>
                                     </div> : null
-                            })()}</div>
+                            }</div>
                         </div>
                         <div className={style.personInfo}>
                             <div className={style.personName}>{props.currentPerson[0].name}</div>
@@ -50,11 +50,7 @@ const PersonPage = (props) => {
                         <img onClick={props.setHomePage} src={require('../../images/logo.png')} alt='logo'/>
                     </div>
                     <div className={style.elementBlock}>
-                        {(() => {
-                            if (props.currentPerson[0].subjectsCount !== 0) {
-                                return <img alt="element"/>
-                            }
-                        })()}
+                        {props.currentPerson[0].subjectsCount !== 0 ? <img alt="element"/> : null}
                     </div>
                     <div className={style.subjectsBlock}>
                         <div className={style.subjects}>
@@ -67,8 +63,8 @@ const PersonPage = (props) => {
                                              src={require('../../images/' + person.image)}
                                              alt={person.name}
                                              width='100'/>
-                                        {(() => {
-                                            return person.subjectsCount !== 0 ?
+                                        {
+                                            person.subjectsCount !== 0 ?
                                                 <div className={style.personSubjectsCountOfSubject}>
                                                     <img className={style.subjectBorderOfSubject}
                                                          src={require('../../images/site/subjectBorder.png')} alt=''/>
@@ -76,7 +72,7 @@ const PersonPage = (props) => {
                                                         className={style.subjectsCountOfSubject}>{person.subjectsCount}</div>
                                                 </div>
                                                 : null
-                                        })()}
+                                        }
                                     </div>
                                     <div className={style.subjectName}>{person.name}</div>
                                     <div className={style.subjectPost}>{person.post}</div>

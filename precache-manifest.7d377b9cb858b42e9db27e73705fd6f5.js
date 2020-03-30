c80955dc96b3727da86af07224d916fc049edfd0
lexent3010 <lexent.lex@gmail.com>
2020-03-31 00:47:04 +0300

    Updates

---
+++ precache-manifest.7d377b9cb858b42e9db27e73705fd6f5.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6468945521b141709056b2bda95de2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909ddca8b6e08b9eeffe",
+    "url": "/static/css/main.3bb7b5e5.chunk.css"
+  },
+  {
+    "revision": "f6f8b057592f40d58c2e",
+    "url": "/static/js/2.ffc96d16.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ffc96d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909ddca8b6e08b9eeffe",
+    "url": "/static/js/main.690eaa17.chunk.js"
+  },
+  {
+    "revision": "51f802b247b7d6caa38f",
+    "url": "/static/js/runtime-main.3a79632d.js"
+  },
+  {
+    "revision": "d87e6ffe3a0ae3bf5e9217e1a602f4ba",
+    "url": "/static/media/1.d87e6ffe.svg"
+  },
+  {
+    "revision": "40ecd72cef0c3cce16cbf9e43f8599ff",
+    "url": "/static/media/2.40ecd72c.svg"
+  },
+  {
+    "revision": "6735cde8f2f5614baa0a5d721945c46c",
+    "url": "/static/media/albrich.6735cde8.jpg"
+  },
+  {
+    "revision": "feb7268c33851e39c5a41a6b4f7a29ee",
+    "url": "/static/media/anahid.feb7268c.jpg"
+  },
+  {
+    "revision": "e57c5e0611e4fd1be23b4053139e32c3",
+    "url": "/static/media/ardal.e57c5e06.jpg"
+  },
+  {
+    "revision": "87da1ffae3a29ec364d5b5901b4f2cbb",
+    "url": "/static/media/auckes.87da1ffa.jpg"
+  },
+  {
+    "revision": "94efc6572dcb6201c378ec9eb291125b",
+    "url": "/static/media/bg1024_1-0.94efc657.jpg"
+  },
+  {
+    "revision": "113892d05bd6eed1da3b3eca745c9ee0",
+    "url": "/static/media/bg1024_2-0.113892d0.jpg"
+  },
+  {
+    "revision": "f1e25d90d1c7e8210bcd6eb398d91cfb",
+    "url": "/static/media/bg1440_1-0.f1e25d90.jpg"
+  },
+  {
+    "revision": "d294201b632d2383cc579174c2476c39",
+    "url": "/static/media/bg1440_2-0.d294201b.jpg"
+  },
+  {
+    "revision": "ae7869e004f62baf05a16214233635c8",
+    "url": "/static/media/bg1440_2-01.ae7869e0.jpg"
+  },
+  {
+    "revision": "bcb5fd05b57ac5ffe8559ee96468dd35",
+    "url": "/static/media/bg768_1-0.bcb5fd05.jpg"
+  },
+  {
+    "revision": "917c4ce070f478ee84a4cb9ff34673e6",
+    "url": "/static/media/bg768_2-0.917c4ce0.jpg"
+  },
+  {
+    "revision": "9c04d432cf309fc747b0ca583a4c01de",
+    "url": "/static/media/borsodi.9c04d432.jpg"
+  },
+  {
+    "revision": "fd6ff0b48c5e99f6eae7fbf4cbd3c9a3",
+    "url": "/static/media/bran.fd6ff0b4.jpg"
+  },
+  {
+    "revision": "919406ce4e364a4633754cf73293a5c1",
+    "url": "/static/media/calveit.919406ce.jpg"
+  },
+  {
+    "revision": "054c3822cfd9d88ab5ce3ef9b35a4cd1",
+    "url": "/static/media/ceallach.054c3822.jpg"
+  },
+  {
+    "revision": "321bb31bcb26ad8e51096752b942fc34",
+    "url": "/static/media/dijkstra.321bb31b.jpg"
+  },
+  {
+    "revision": "57c49e38607d719c3c6f014c915ddce2",
+    "url": "/static/media/dyffryn.57c49e38.jpg"
+  },
+  {
+    "revision": "3acc38e5d938c64c5b684ad1faf61deb",
+    "url": "/static/media/emhyr.3acc38e5.jpg"
+  },
+  {
+    "revision": "9b7d57de006aa1b450167284bde2645e",
+    "url": "/static/media/everec.9b7d57de.jpg"
+  },
+  {
+    "revision": "de85588a5a0c28b36fd1d4b25d64c81f",
+    "url": "/static/media/fringilla.de85588a.jpg"
+  },
+  {
+    "revision": "3e3f8ff6188073bc4c5c3da572dbb02b",
+    "url": "/static/media/gwynleve.3e3f8ff6.jpg"
+  },
+  {
+    "revision": "e49fd43404d4bb09cb7bf338b4929360",
+    "url": "/static/media/horst.e49fd434.jpg"
+  },
+  {
+    "revision": "58d1f06a56b602ac1bf70d54d5477244",
+    "url": "/static/media/iris.58d1f06a.jpg"
+  },
+  {
+    "revision": "ce050318c05f81beef5c94bd7759be8b",
+    "url": "/static/media/joachim.ce050318.jpg"
+  },
+  {
+    "revision": "9f7dffda41b7b2cdcc425dc59196df31",
+    "url": "/static/media/letho.9f7dffda.jpg"
+  },
+  {
+    "revision": "4dd96e032130ac2dd7fd064cadb4f433",
+    "url": "/static/media/logo.4dd96e03.png"
+  },
+  {
+    "revision": "7b0e31b136f29f8db89c36bcd4d14b09",
+    "url": "/static/media/menno.7b0e31b1.jpg"
+  },
+  {
+    "revision": "12a4417d01d55d6327731c40f23e15ac",
+    "url": "/static/media/morvran.12a4417d.jpg"
+  },
+  {
+    "revision": "ac02c2a5a23de44cb1bc18a9527c8da5",
+    "url": "/static/media/nilfgaard.ac02c2a5.jpg"
+  },
+  {
+    "revision": "63af5e3cdeb79dd239e79f189f608107",
+    "url": "/static/media/olgierd.63af5e3c.jpg"
+  },
+  {
+    "revision": "dd42530b0cfc0a7b5a508fe7e4bf6fc6",
+    "url": "/static/media/philippa.dd42530b.jpg"
+  },
+  {
+    "revision": "6e844d6fc6c2da873b2c6a22bd0d420e",
+    "url": "/static/media/radovid.6e844d6f.jpg"
+  },
+  {
+    "revision": "29aee07a43bb6ff4512dc121807a233c",
+    "url": "/static/media/redania.29aee07a.jpg"
+  },
+  {
+    "revision": "1aab5977be44b5315a8eb745a6e8964d",
+    "url": "/static/media/shilard.1aab5977.jpg"
+  },
+  {
+    "revision": "05d10ee31ec669de1b78e977dcebf63f",
+    "url": "/static/media/skellen.05d10ee3.jpg"
+  },
+  {
+    "revision": "e5f5f18f5cb5425b350927ff07cfde60",
+    "url": "/static/media/skellige.e5f5f18f.jpg"
+  },
+  {
+    "revision": "ef210f970603479272471ed528e3aa3e",
+    "url": "/static/media/subjectBorder.ef210f97.png"
+  },
+  {
+    "revision": "a40245e12dacb8556a413b1ce7b35c6f",
+    "url": "/static/media/sweers.a40245e1.jpg"
+  },
+  {
+    "revision": "0ef5d5e33c6440af84df32b474078f73",
+    "url": "/static/media/tibor.0ef5d5e3.jpg"
+  },
+  {
+    "revision": "f62ccf61786459cb5f498aa6d69c2676",
+    "url": "/static/media/vanhemar.f62ccf61.jpg"
+  },
+  {
+    "revision": "779ba4dd14de430fea0aa7fda4583616",
+    "url": "/static/media/vattier.779ba4dd.jpg"
+  },
+  {
+    "revision": "12ba32edd41092c74133d4f3029a91e2",
+    "url": "/static/media/vicovaro.12ba32ed.jpg"
+  },
+  {
+    "revision": "d462f4a845b5949302b836b318d50fbd",
+    "url": "/static/media/vilgefortz.d462f4a8.jpg"
+  },
+  {
+    "revision": "4f5879fa745bd920540ef72750d4f14b",
+    "url": "/static/media/xarthisius.4f5879fa.jpg"
+  }
+]);
